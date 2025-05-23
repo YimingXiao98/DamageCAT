c977cf3d6a939ecc76a09a665dcb43f678a05b1b
YimingXiao98 <yxiao@tamu.edu>
2025-05-23 15:43:32 -0500

    Included requirements.txt

--- scripts/run_cd.sh
+++ scripts/run_cd.sh
@@ -15,10 +15,10 @@ batch_size=8
 max_epochs=300
 net_G=newUNetTrans
 
-random_seed=2025
+random_seed=2026
 
 split=train  
 split_val=val  
-project_name=CROP_${net_G}_${data_name}_b${batch_size}_lr${lr}_${split}_${split_val}_300_${lr_policy}_ce_dice_smoothen_seed${random_seed}
+project_name=seed${random_seed}_${net_G}_${data_name}_b${batch_size}_lr${lr}_${split}_${split_val}_300_${lr_policy}_ce_dice_smoothen
 
 python main_cd.py --img_size ${img_size} --checkpoint_root ${checkpoint_root} --lr_policy ${lr_policy} --split ${split} --split_val ${split_val} --net_G ${net_G} --gpu_ids ${gpus} --max_epochs ${max_epochs} --project_name ${project_name} --batch_size ${batch_size} --data_name ${data_name}  --lr ${lr} --dataset ${dataset} --loss ${loss} --n_class ${n_class} --random_seed ${random_seed}
